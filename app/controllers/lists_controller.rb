89b1b7441e0e0811495eccedcb465fd4eb5c53c2
Dominic M. Liddell <speak@dominicmliddell.com>
2014-05-23 16:03:51 -0500

    can add new list, view lists associated with user

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,16 +2,18 @@ class ListsController < ApplicationController
 
   #index
   def index
+     @user = User.find(params[:user_id])
     end
 
   #show
   def show
+     @list = List.find(params[:id])
     end
 
   #create
   def create
     @user = User.find(params[:user_id])
-    @list = @user.lists.create(params[:list].permit(:title, :description))
+    # @lists = List.find(params[:user_id])
   end
 
    
